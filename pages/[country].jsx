6806846601faafc96e46cc34a90cb2550e2218b3
unknown <marko.rodriguez@hotmail.es>
2022-01-22 23:51:51 -0500

    404 error handled

--- pages/[country].jsx
+++ pages/[country].jsx
@@ -3,14 +3,20 @@ import Link from "next/link"
 
 const selectedCountry = ({ data, arrayBorderNames }) => {
     return (
-        <Layout title={data[0].name.common + ' | Countries API'}>
+    <>
+        {data.status == '404' ? <Layout title={'Error | Countries API'}>
+            <div className="my-14">
+            <img className="mx-auto" src="https://http.cat/404" alt="code_cat" />
+            </div>
+            
+        </Layout> : <Layout title={ '| Countries API'}>
             <Link href="/">
                 <button className="px-4 py-1 my-14 bg-dark_blue rounded-sm text-custom_white"> ← Back </button>
             </Link>
 
             <div className="grid gap-12 grid-cols-1 md:grid-cols-2 grid-rows-1">
                 <div>
-                    <img className="h-full" src={data[0].flags.svg} alt="" />
+                    <img className="h-full" src={data[0].flags.svg}  alt="" />
                 </div>
                 <div className="py-8 text-custom_white">
                     <h1 className="text-3xl font-semibold">{data[0].name.common}</h1>
@@ -18,10 +24,10 @@ const selectedCountry = ({ data, arrayBorderNames }) => {
                         <div className="md:w-3/6 w-full">
                             <h3 className="my-2"> <span className="font-semibold"> Official name : </span> {data[0].name.official}  </h3>
                             <h3 className="my-2"> <span className="font-semibold"> Population : </span> {data[0].population.toLocaleString("en-US")}  </h3>
-                            <h3 className="my-2"> <span className="font-semibold"> Region : </span> {data[0].region}  </h3>
-                            <h3 className="my-2"> <span className="font-semibold"> Subregion : </span> {data[0].subregion}  </h3>
+                            <h3 className="my-2"> <span className="font-semibold"> Region : </span> {data[0].region? data[0].region : '---'}  </h3>
+                            <h3 className="my-2"> <span className="font-semibold"> Subregion : </span> {data[0].subregion ? data[0].subregion : ' --- '}  </h3>
                             <h3 className="my-2"> <span className="font-semibold"> Capital : </span> {
-                                data[0].capital ? data[0].capital : null
+                                data[0].capital ? data[0].capital : ' --- '
                             }  </h3>
                         </div>
                         <div className="md:w-3/6 w-full">
@@ -29,15 +35,15 @@ const selectedCountry = ({ data, arrayBorderNames }) => {
                                 data[0].tld ? data[0].tld[0] : ' not provided '
                             }  </h3>
                             <h3 className="my-2"> <span className="font-semibold"> Currencies : </span> {
-                               data[0].currencies ? data[0].currencies[Object.keys(data[0].currencies)[0]].name : ' not provided '
+                               data[0].currencies ? data[0].currencies[Object.keys(data[0].currencies)[0]].name : '--- '
                             }  </h3>
                             <h3 className="my-2"> <span className="font-semibold"> Languages : </span> {
-                                Object.values(data[0].languages).toString()
+                                data[0].languages ? Object.values(data[0].languages).toString() : ' --- '
                             }  </h3>
                         </div>
                     </div>
 
-                    <h3 className="my-2"> <span className="font-semibold"> Border countries : </span> {data[0].borders ? arrayBorderNames.map((el, index)=>(
+                    <h3 className="my-2"> <span className="font-semibold"> Border countries : </span> { data[0].borders ? arrayBorderNames.map((el, index)=>(
                         <Link key={index} href={`/${el}`}>
                           <a>  <button  className="mx-4 my-4 py-1 px-4 bg-dark_blue shadow-md "> {el} </button> </a> 
                         </Link>
@@ -46,8 +52,10 @@ const selectedCountry = ({ data, arrayBorderNames }) => {
 
                 </div>
             </div>
-        </Layout>
-    );
+        </Layout> }
+    </>
+        
+);
 };
 
 export default selectedCountry;
@@ -72,7 +80,7 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
     try {
         const res = await fetch(
-            "https://restcountries.com/v3.1/name/" + params.country +"?fullText=true"
+            "https://restcountries.com/v3.1/name/" + `${params.country}` +"?fullText=true"
         );
         const data = await res.json();
         
@@ -81,16 +89,22 @@ export async function getStaticProps({ params }) {
         
         const arrayBorderNames = []
 
-        const result = dataGlobal.find(element => element.cca3 == data[0].cca3)
+        console.log(data)
+
+       // console.log(data[0].borders)
+
+        //const result = dataGlobal.find(element => element.cca3 == data[0].cca3)
         
+       // console.log(result.borders)
+/*
         if (result.borders != null ) {
             result.borders.map((el)=>{
                 const borderCountry = dataGlobal.find(country => country.cca3 == el )
                 arrayBorderNames.push(borderCountry.name.common)
             })
         } 
-        
-        console.log(data)
+*/
+        //console.log(data)
 
         return {
             props: {
